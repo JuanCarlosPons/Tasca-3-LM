18cf10aec01956adf14cd908111888f70a98a936
JuanCarlosPons <juankete3@hotmail.com>
2017-01-30 21:47:43 +0100

    Actualización código js

--- js/js.js
+++ js/js.js
@@ -1,14 +1,14 @@
-var formElement=null;
+//var formElement=null;
 
-window.onload = function(){
+//window.onload = function(){
 	
 	 //pide los datos, lee preguntas.xml del servidor (por http)
- var xhttp = new XMLHttpRequest();
- xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-   gestionarXml(this);
+ //var xhttp = new XMLHttpRequest();
+ //xhttp.onreadystatechange = function() {
+  //if (this.readyState == 4 && this.status == 200) {
+   //gestionarXml(this);
   }
  };
- xhttp.open("GET", "xml/questions.xml", true);
- xhttp.send();
+ //xhttp.open("GET", "xml/questions.xml", true);
+ //xhttp.send();
 }
