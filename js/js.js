4216413a553c74c56e356d2c37c854f665c8a44e
JuanCarlosPons <juankete3@hotmail.com>
2017-01-30 21:40:08 +0100

    Subidos los archivos y carpetas de prueba
    Se ha creado una rama de prueba con este commit

---
+++ js/js.js
@@ -0,0 +1,14 @@
+var formElement=null;
+
+window.onload = function(){
+	
+	 //pide los datos, lee preguntas.xml del servidor (por http)
+ var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   gestionarXml(this);
+  }
+ };
+ xhttp.open("GET", "xml/preguntas.xml", true);
+ xhttp.send();
+}
