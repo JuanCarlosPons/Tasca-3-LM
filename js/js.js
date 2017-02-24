31368ca27d7696f49199972a149581a2810377f6
JuanCarlosPons <juankete3@hotmail.com>
2017-02-24 23:10:21 +0100

    Pujar arxius

---
+++ js/js.js
@@ -0,0 +1,12 @@
+window.onload = function(){ 
+ 
+ //LEER XML de xml/questions.xml
+ var xhttp = new XMLHttpRequest();
+ xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+   gestionarXml(this);
+  }
+ };
+ xhttp.open("GET", "xml/questions.xml", true);
+ xhttp.send();
+}
