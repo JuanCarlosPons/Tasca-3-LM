85ed78130591f726aa5f0db4ab222b159665bcd8
JuanCarlosPons <juankete3@hotmail.com>
2017-02-25 19:31:11 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,6 +1,11 @@
+
+//**************************************************************************************************** 
+//Después de cargar la página (onload) se definen los eventos sobre los elementos, entre otras acciones.
+
 window.onload = function(){ 
  
  //LEER XML de xml/questions.xml
+ 
  var xhttp = new XMLHttpRequest();
  xhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
@@ -11,12 +16,14 @@ window.onload = function(){
  xhttp.send();
 }
 
+//****************************************************************************************************
+// Recuperamos los datos del fichero XML xml/preguntas.xml
+// xmlDOC es el documento leido XML.
 function gestionarXml(dadesXml){
- var xmlDoc = dadesXml.responseXML; 
- 
+ var xmlDoc = dadesXml.responseXML; //Parse XML to xmlDoc
 
- //Recuperamos el título y las opciones de cada question del XML y guardamos las respuestas correctas
- //Pregunta 3 - SELECT 1
+ //SELECT1
+ //Recuperamos el título y las opciones, guardamos la respuesta correcta
  var pregunta003=xmlDoc.getElementsByTagName("title")[2].innerHTML;
  var opcionesSelect1 = [];
  var nopt = xmlDoc.getElementById("profe003").getElementsByTagName('option').length;
@@ -26,17 +33,20 @@ function gestionarXml(dadesXml){
  ponerDatosSelectHtml(pregunta003,opcionesSelect1);
  respuestaSelect1=parseInt(xmlDoc.getElementsByTagName("answer")[2].innerHTML);
  
- //Pregunta 4 - SELECT 2
+ //SELECT1
+ //Recuperamos el título y las opciones, guardamos la respuesta correcta
  var pregunta004=xmlDoc.getElementsByTagName("title")[3].innerHTML;
  var opcionesSelect2 = [];
  var nopt = xmlDoc.getElementById("profe004").getElementsByTagName('option').length;
   for (i = 0; i < nopt; i++) { 
     opcionesSelect2[i] = xmlDoc.getElementById("profe004").getElementsByTagName('option')[i].innerHTML;
  }
- ponerDatosSelectHtml1(pregunta004,opcionesSelect2);
+ ponerDatosSelectHtml2(pregunta004,opcionesSelect2);
  respuestaSelect2=parseInt(xmlDoc.getElementsByTagName("answer")[2].innerHTML);
 }
 
+//****************************************************************************************************
+// poner los datos recibios en el HTML
 function ponerDatosSelectHtml1(t,opt){
   document.getElementById("pregunta003").innerHTML=t;
   var select = document.getElementsByTagName("select")[0];
